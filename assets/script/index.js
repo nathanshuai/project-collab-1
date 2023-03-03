bf9f774fe7f7ae9f051c987cf3845da96fc06290
Jayvee Milana <jmmilana12@gmail.com>
2023-03-03 14:18:35 -0600

    form js start

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,5 +1,9 @@
 'use strict';
 
 const year = document.querySelector('.year');
+const name = document.querySelector('.name');
+const email = document.querySelector('.email');
+const phone = document.querySelector('.phone');
+const message = document.querySelector('.message');
 
 year.innerText = new Date().getFullYear();
